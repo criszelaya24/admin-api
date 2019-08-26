5c2d93fcfcd1637b8965bd7692711cb4fedd07a8
Cristopher Palacios <criszelaya24@hotmail.com>
2019-08-26 08:51:36 -0600

    Hanlder creation and validaing if is user admin or not

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   scope '/app/v1' do
     post 'user_token' => 'user_token#create'
     resources :users
+    resources :time_records
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
