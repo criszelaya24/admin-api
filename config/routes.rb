a36520de34e9f405cf83bbaae1cd4ca62f26f19d
Cristopher Palacios <criszelaya24@hotmail.com>
2019-08-26 08:25:34 -0600

    Renaming routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  post 'user_token' => 'user_token#create'
-  resources :users
+  scope '/app/v1' do
+    post 'user_token' => 'user_token#create'
+    resources :users
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
