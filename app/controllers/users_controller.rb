d94ca703b75172a286dbc82c117b990423b7f0ba
Cristopher Palacios <criszelaya24@hotmail.com>
2019-08-26 08:31:26 -0600

    Using clause guards

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,11 +18,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    if @user.save
-      render json: @user, status: :created, location: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
+    render json: @user, status: :created, location: @user if @user.save
+    render json: @user.errors, status: :unprocessable_entity unless @user.save
   end
 
   # PATCH/PUT /users/1
