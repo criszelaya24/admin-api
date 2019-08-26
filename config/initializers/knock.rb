40990749471db489822835b769227b6a3c4133b8
Cristopher Palacios <criszelaya24@hotmail.com>
2019-08-26 16:16:21 -0600

    settinng up life time of token

--- config/initializers/knock.rb
+++ config/initializers/knock.rb
@@ -1,5 +1,6 @@
 Knock.setup do |config|
 
+  config.token_secret_signature_key = -> { Rails.application.credentials.read }
   ## Expiration claim
   ## ----------------
   ##
@@ -7,7 +8,7 @@ Knock.setup do |config|
   ## last forever.
   ##
   ## Default:
-  # config.token_lifetime = 1.day
+  config.token_lifetime = 1.day
 
 
   ## Audience claim
